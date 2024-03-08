a7084e6f9c0fc4491922f89da79246eb18eb3cb2
s27799 <s27799@pjwstk.edu.pl>
2024-03-08 15:17:53 +0100

    Added GetAverage method

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello test commit 1");
-Console.WriteLine("Hello test commit 2");
-Console.WriteLine("Hello test commit 3");
+static double GetAverage(int[] numbers)
+{
+    double result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result += numbers[i];
+    }
+
+    return result / numbers.Length;
+}
