a490ec0ed7fb52d1ca6486575948da0371e7344c
s27799 <s27799@pjwstk.edu.pl>
2024-03-08 15:21:32 +0100

    Added some code

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -10,3 +10,7 @@ static double GetAverage(int[] numbers)
 
     return result / numbers.Length;
 }
+
+int[] numbers = { 1, 2, 3, 4};
+double average = GetAverage(numbers);
+Console.WriteLine(average);
